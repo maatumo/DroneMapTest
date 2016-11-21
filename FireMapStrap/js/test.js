23615c3167bd867c8f53497eabbebfcb3784d027
maatumo <opus_aqua@hotmail.co.jp>
2016-11-22 01:24:31 +0900

    alert system

--- FireMapStrap/js/test.js
+++ FireMapStrap/js/test.js
@@ -3,6 +3,12 @@ var file;
 var body;
 var bodyType;
 var state=0;//0が観測者モード、1がpilot modeもで自分の位置情報を数秒に一回発信
+
+debugList=[];
+debugList[3]='test';
+
+console.log(debugList);
+
 if(localStorage){
 	//機体名の取得
 	body=localStorage.getItem('myBodyName');
@@ -24,12 +30,15 @@ var mymarker;
 var marker = [];
 var infoWindow = [];
 var markerData = [];
+var alertAreas=[];
+var	alertAreas2=[];
+
 // Maps
 	var map;
 	var tokyo;
 	var pos;
 
-	function initMap() {
+	function initMap() {//keyを取得してgoogle maps apiのsourceを取得した後に発火する
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 32.4, lng: 130.2},
 			zoom: 12
@@ -38,29 +47,64 @@ var markerData = [];
 		tokyo = new google.maps.LatLng(35.689614,139.691585);
 		pos = new google.maps.LatLng(34.686272,135.519649);
 
+		//image設定
+		drone_image={url:'drone_mini.png',size: new google.maps.Size(50, 50),
+			origin: new google.maps.Point(0, 0),anchor: new google.maps.Point(25, 25)};
+		heli_image={url:'heli_mini.png',size: new google.maps.Size(90, 67),
+			origin: new google.maps.Point(0, 0),anchor: new google.maps.Point(45, 33)};
+
+
+
+		//すでにある機体データをロードして表示
 		for (var i = 0; i < markerData.length; i++) {
 			var iconType;
 			if(markerData[i]['bodyType']=="Multicopter"){
-				iconType='drone_mini.png';
+				iconType=drone_image;
 			}else if(markerData[i]['bodyType']=="Helicopter"){
-				iconType='heli_mini.png';
+				iconType=heli_image;
 			}
 	        markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
     	    marker[i] = new google.maps.Marker({ // マーカーの追加
         	    position: markerLatLng, // マーカーを立てる位置を指定
            	 	map: map, // マーカーを立てる地図を指定
 	        	icon: iconType,//アイコン指定
+	        	anchor: 0.5,
 	        });
 	        infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
     	        content: '<div class="sample">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
         	});
 	        markerEvent(i); // マーカーにクリックイベントを追加
-	    }	 
+
+
+			// 危険エリアの表示
+			alertAreas[i]= new google.maps.Circle({
+			  center: markerLatLng,       // 中心点(google.maps.LatLng)
+			  fillColor: '#00ff00',   // 塗りつぶし色
+			  fillOpacity: 0.2,       // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+			  map: map,        // 表示させる地図（google.maps.Map）
+			  radius: 900,          // 半径（ｍ）
+			  strokeColor: '#00ff00', // 外周色 
+			  strokeOpacity: 1,       // 外周透過度（0: 透明 ⇔ 1:不透明）
+			  strokeWeight: 1         // 外周太さ（ピクセル）
+			});
+			// 危険エリアの表示
+			alertAreas2[i]= new google.maps.Circle({
+			  center: markerLatLng,       // 中心点(google.maps.LatLng)
+			  fillColor: '#00ff00',   // 塗りつぶし色
+			  fillOpacity: 0.2,       // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+			  map: map,        // 表示させる地図（google.maps.Map）
+			  radius: 1800,          // 半径（ｍ）
+			  strokeColor: '#00ff00', // 外周色 
+			  strokeOpacity: 1,       // 外周透過度（0: 透明 ⇔ 1:不透明）
+			  strokeWeight: 1         // 外周太さ（ピクセル）
+			});
+	    }
+
 
 
 		//自分の位置のマーカー
 		
-		if(mymarker){		mymarker.setMap(null);}
+		if(mymarker){mymarker.setMap(null);}
 		mymarkerLatLng = new google.maps.LatLng({lat: myPos["lat"], lng: myPos["lng"]}); // 緯度経度のデータ作成
 		mymarker = new google.maps.Marker({ // マーカーの追加
 		position: mymarkerLatLng, // マーカーを立てる位置を指定
@@ -85,11 +129,9 @@ var markerData = [];
 			  	updateTime: date.getFullYear()  + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
 				});
 			}
+
 		});
-
-
-
-	    
+		
 	}
 
 
@@ -120,7 +162,8 @@ var gkey;
 dataStore.child('key').on('child_added',function(dataSnapShot){
 	gkey = dataSnapShot.val();
 	console.log("gkey"+gkey);
-	var src= "https://maps.googleapis.com/maps/api/js?key="+gkey+"&callback=initMap";
+//	var src="http://maps.google.com/maps/api/js?sensor=false&libraries=geometry"
+	var src= "https://maps.googleapis.com/maps/api/js?key="+gkey+"&libraries=geometry&callback=initMap";
 	var scriptElement = document.createElement('script');
 	scriptElement.src = src ;
 	document.getElementsByTagName('head')[0].appendChild(scriptElement);
@@ -136,7 +179,7 @@ airplanes.on('value',function(dataSnapShot){
 	console.log('data from firebase!');
 	console.log(data);
 	markerData = [];
-	for(var bodyname in data){
+	for(var bodyname in data){//firebaseからのデータをMarkerDataに格納
 		console.log(bodyname+" data "+data[bodyname]);
 		markerData.push({
 	        name: bodyname,
@@ -146,16 +189,28 @@ airplanes.on('value',function(dataSnapShot){
 	        updateTime: data[bodyname]["updateTime"]
 	    });
 	}
+	//更新時にマーカーとかを消す http://www.ajaxtower.jp/googlemaps/gmarker/index4.html
 	for (var i = 0; i < marker.length; i++) {
 		marker[i].setMap(null);
 	}
+	for (var i = 0; i < marker.length; i++) {
+		alertAreas[i].setMap(null);
+	}
+	for (var i = 0; i < marker.length; i++) {
+		alertAreas2[i].setMap(null);
+	}
 	marker=[];
-	for (var i = 0; i < markerData.length; i++) {
+	alertAreas=[];
+	alertAreas2=[];
+	distance=[];//他機との距離のリスト（自分の機体の時はスルー）
+	alertMessages=[];
+	cationMessages=[];
+	for (var i = 0; i < markerData.length; i++) {//各機体に関するループ
 		var iconType;
 		if(markerData[i]['bodyType']=="Multicopter"){
-			iconType='drone_mini.png';
+			iconType=drone_image;
 		}else if(markerData[i]['bodyType']=="Helicopter"){
-			iconType='heli_mini.png';
+			iconType=heli_image;
 		}
 		markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
 		marker[i] = new google.maps.Marker({ // マーカーの追加
@@ -168,7 +223,62 @@ airplanes.on('value',function(dataSnapShot){
 		content: '<div class="sample">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
 		});
 		markerEvent(i); // マーカーにクリックイベントを追加
+
+		// 危険エリアの表示
+		// 他機との中心間の距離のけいさん
+		//自分の機体の時は発火せず
+		var circle_color='#ff0000';
+		if(body!=markerData[i]['name']){
+			distance[i] = google.maps.geometry.spherical.computeDistanceBetween(markerLatLng, mymarkerLatLng);
+			if(distance[i]<1800){
+				alertMessages[i]=markerData[i]['name'];
+				circle_color='#ff0000';
+			}else if(distance[i] >= 1800 && distance[i]<3600){
+				circle_color='#ccff00';
+				cationMessages[i]=markerData[i]['name'];
+			}else{
+				circle_color='#33ccff';				
+			}
+			console.log(markerData[i]['name']+" : "+distance[i]);
+		}
+
+		alertAreas[i]= new google.maps.Circle({
+		  center: markerLatLng,       // 中心点(google.maps.LatLng)
+		  fillColor: circle_color,  // 塗りつぶし色
+		  fillOpacity: 0.2,       // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+		  map: map,        // 表示させる地図（google.maps.Map）
+		  radius: 900,          // 半径（ｍ）
+		  strokeColor: circle_color, // 外周色 
+		  strokeOpacity: 1,       // 外周透過度（0: 透明 ⇔ 1:不透明）
+		  strokeWeight: 1         // 外周太さ（ピクセル）
+		});
+
+		alertAreas2[i]= new google.maps.Circle({
+		  center: markerLatLng,       // 中心点(google.maps.LatLng)
+		  fillColor: circle_color,  // 塗りつぶし色
+		  fillOpacity: 0.2,       // 塗りつぶし透過度（0: 透明 ⇔ 1:不透明）
+		  map: map,        // 表示させる地図（google.maps.Map）
+		  radius: 1800,          // 半径（ｍ）
+		  strokeColor: circle_color, // 外周色 
+		  strokeOpacity: 1,       // 外周透過度（0: 透明 ⇔ 1:不透明）
+		  strokeWeight: 1         // 外周太さ（ピクセル）
+		});
+		
 	}
+	console.log("alerts");
+	console.log(alertMessages);
+	var at=document.getElementById('alertText');
+	if(alertMessages.length!=0){
+		at.innerHTML = "ALERT: <1 mile [Land Your Drone!]";
+		at.style.background="#ff0000";
+	}else if(cationMessages.length!=0){
+		at.innerHTML = "ALERT: <2 mile";
+		at.style.background="#ccff00";
+	}else{
+		at.innerHTML = "Saftey Flight: >2 mile";
+		at.style.background="#55bbff";
+	}
+
 
 });
 
